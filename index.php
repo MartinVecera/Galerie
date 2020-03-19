b9d13da26772ec914f8df2db46e0b0319eee5334
MartinVecera <36948869+MartinVecera@users.noreply.github.com>
2020-03-19 17:25:15 +0100

    složky

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang='cs'>
+<head>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <!-- UIkit CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.3.7/dist/css/uikit.min.css" />
+
+    <!-- UIkit JS -->
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.3.7/dist/js/uikit.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.3.7/dist/js/uikit-icons.min.js"></script>
+    <title>Index</title>
+</head>
+<body>
+    
+</body>
+</html>
